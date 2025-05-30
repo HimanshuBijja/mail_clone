bb717760c0422ab8f5dc44f17a0aefef5fed2184
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-05-30 22:24:54 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       <ChevronDown />
       <ChevronUp />
       hello
-      <div className='bg-line'>
+      <div className='text-line'>
         wassup
       </div>
     </div>
