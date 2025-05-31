6ba54404322598065eeb71a1226b13fee37f5da3
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-05-31 11:20:24 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
-import { Star } from "lucide-react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Inbox } from "./pages/Inbox";
 import "./App.css";
-import Message from "./pages/message";
-import Nav from "./pages/Nav";
+
+
+
 function App() {
     return (
-        <div className="">
-            <Nav />
-
-            <div className="flex flex-row items-center justify-between mx-4 my-5 mb-7 ">
-                <h1 className="text-2xl font-medium text-foreground">
-                    Message
-                </h1>
-                <Star />
-            </div>
-            <Message />
-            <hr className="text-secondary/20" />
-            <Message />
-        </div>
-    );
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Inbox />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App;
--- src/pages/text.tsx
+++ src/pages/text.tsx
@@ -1,6 +1,6 @@
 export default function Text() {
     return (
-        <div className="mt-5 mb-2 text-foreground text-lg" style={{fontSize: 16, fontWeight: 500}}>
+        <div className="mt-5 mb-2 text-foreground text-lg no-underline " style={{fontSize: 15, fontWeight: 550}}>
             Name : Himanshu     <br />
             Rolln    : 201900000 <br />
             Reason : passport application
